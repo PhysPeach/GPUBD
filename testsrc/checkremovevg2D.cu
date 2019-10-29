0635be94ab3871ba255618337342517883628279
Masato SHUKAWA <Masato.Shukawa@outlook.jp>
2019-10-30 02:36:39 +0900

    重心運動を引く関数のテスト
    問題なし、原因はtEvoにありか？

---
+++ testsrc/checkremovevg2D.cu
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+
+#include "../h/MT.h"
+
+#include "../h/particles.cuh"
+#include "../h/parameters.cuh"
+
+uint IDs;
+uint IDe;
+float tau;
+float Tfin;
+
+using namespace PhysPeach;
+
+__global__ void setVec(float* x, float a){
+    uint i_global = blockIdx.x * blockDim.x + threadIdx.x;
+    for(uint i = i_global; i < D * NP; i += NB * NT){
+        x[i] = i;
+    }
+}
+
+int main(){
+    std::cout << "hello, test" << std::endl;
+
+    //test
+    Tfin = 1;
+    tau = 100;
+    IDs = 0;
+    IDe = 0;
+
+    //initialise random func
+    init_genrand((unsigned long)time(NULL));
+
+    std::cout << "---Settings---" << std::endl;
+    std::cout << "Tfin = " << Tfin << std::endl;
+    std::cout << "t_eq = " << tau << std::endl;
+    std::cout << "t_rec = " << tau << std::endl;
+    std::cout << "ID = [" << IDs << ", " << IDe << "]" << std::endl;
+    std::cout << "--------------" << std::endl;
+
+    float v1[D*NP];
+    float v2[D*NP];
+
+    Particles p;
+    makeParticles(&p);
+    setVec<<<NB,NT>>>(p.v_dev,6);
+    cudaMemcpy(v1, p.v_dev, D*NP*sizeof(float),cudaMemcpyDeviceToHost);
+    removevg2D(&p);
+    cudaMemcpy(v2, p.v_dev, D*NP*sizeof(float),cudaMemcpyDeviceToHost);
+    killParticles(&p);
+    for(uint i = 0; i < NP; i++){
+        std::cout << i << ": " << v1[i] <<" -> v1 - vg(~=NP/2) = " << v2[i] << std::endl;
+    }
+
+    return 0;
+}
