06d37bbc5aef8ee5f7791ed74fecd10717b8bb4e
Masato SHUKAWA <Masato.Shukawa@outlook.jp>
2019-11-01 13:35:29 +0900

    線形プロットとlogプロット出来るようにした

--- c/box.cu
+++ c/box.cu
@@ -32,13 +32,15 @@ namespace PhysPeach{
     }
 
     void prepareBox(Box* box){
-        box->logFile << "Set InitPositions" << std::endl;
+        std::cout << "Set InitPositions" << std::endl;
         scatterParticles(&box->p, box->L);
         cudaMemcpy(box->p.diam, box->p.diam_dev, NP * sizeof(float),cudaMemcpyDeviceToHost);
         for(uint n = 0; n < NP; n++){
             box->positionFile << box->p.diam[n] << " ";
+            box->animeFile << box->p.diam[n] << " ";
         }
         box->positionFile << std::endl << std::endl;
+        box->animeFile << std::endl << std::endl;
         //set posMem and list
         setIntVecZero<<<NB,NT>>>(box->g.cell_dev, box->g.M * box->g.M * box->g.EpM);
         updateGrid2D<<<NB,NT>>>(box->g, box->g.cell_dev, box->p.x_dev);
@@ -48,7 +50,7 @@ namespace PhysPeach{
             harmonicEvoBox(box);
         }
         
-        box->logFile << "-> SIP Done!" << std::endl;
+        std::cout << "-> SIP Done!" << std::endl;
         return;
     }
     void initBox(Box* box, uint ID){
@@ -59,18 +61,15 @@ namespace PhysPeach{
         std::ostringstream positionFileName;
         positionFileName << "../pos/N" << (uint)NP << "/T" << Tfin << "/posBD_N" << (uint)NP << "_T" << Tfin << "_id" << box->id <<".data";
         box->positionFile.open(positionFileName.str().c_str());
-        std::ostringstream logFileName;
-        logFileName << "../log/N" << (uint)NP << "/T" << Tfin << "/logBD_N" << (uint)NP << "_T" << Tfin << "_id" << box->id <<".log";
-        box->logFile.open(logFileName.str().c_str());
-
-        box->logFile << "Start Initialisation: ID = " << box->id << std::endl;
-        box->logFile << "Created Box ID = " << box->id << std::endl;
+        std::ostringstream animeFileName;
+        animeFileName << "../anime/N" << (uint)NP << "/T" << Tfin << "/animeBD_N" << (uint)NP << "_T" << Tfin << "_id" << box->id <<".log";
+        box->animeFile.open(animeFileName.str().c_str());
         
         setdt_T(box, dt_INIT, Tfin);
         prepareBox(box);
         setdt_T(box, dt_BD, Tfin);
         equilibrateBox(box, tmax);
-        box->logFile << "-> Init Done!" << std::endl;
+        std::cout << "-> Init Done!" << std::endl;
         return;
     }
 
@@ -117,7 +116,51 @@ namespace PhysPeach{
 	    for (uint nt = 0; nt < Nt; nt++) {
 		    tEvoBox(box);
 	    }
-	    std::cout << " -> Edone: ID = "<< box->id << std::endl;
+	    std::cout << " -> Edone"<< box->id << std::endl;
+        return;
+    }
+
+    //record
+    void recBox(std::ofstream *of, Box* box){
+        cudaMemcpy(box->p.x, box->p.x_dev, D * NP * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(box->p.v, box->p.v_dev, D * NP * sizeof(float), cudaMemcpyDeviceToHost);
+        *of << box->t << " " << K(&box->p) << " " << U(&box->g, box->p.diam_dev, box->p.x_dev) << " ";
+	    for (int n = 0; n < NP; n++) {
+		    for (char d = 0; d < D; d++) {
+			    *of << box->p.x_dev[d*NP+n] << " ";
+            }
+            for (char d = 0; d < D; d++) {
+			    *of << box->p.v_dev[d*NP+n] << " ";
+            }
+        }
+	    *of << std::endl;
+        return;
+    }
+    void getData(Box* box){
+        std::cout << "Starting time loop: ID = " << box->id << std::endl;
+        uint Nt, tag;
+        std::cout << "getting anime datas in 10secs" << std::endl;
+        Nt = 10./box->dt;
+        tag = 0;
+        for(uint nt = 0; nt <=Nt; nt++){
+            tEvoBox(box);
+            if(nt >= tag){
+                box->t = nt * box->dt;
+                recBox(&box->animeFile, box);
+                tag += tag = 0.1/box->dt;
+            }
+        }
+        std::cout << "getting logPlot datas" << std::endl;
+        Nt = tmax/box->dt;
+        tag = 10;
+        for(uint nt = 0; nt <= Nt; nt++){
+            tEvoBox(box);
+            if(nt >= tag){
+                box->t = nt * box->dt;
+                recBox(&box->positionFile, box);
+                tag *= 1.1;
+            }
+        }
         return;
     }
 }
